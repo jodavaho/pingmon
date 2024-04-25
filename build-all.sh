292299570ad01742e4b3713eb5f292d35570862b
Josh Vander Hook <josh@vanderhook.info>
2024-04-25 15:29:52 -0500

    Working on release deb building

--- build-all.sh
+++ build-all.sh
@@ -1,1 +1,4 @@
-cargo build --target x86_64-pc-windows-gnu
+cross build --release --target x86_64-pc-windows-gnu
+cross build --release --target x86_64-unknown-linux-gnu
+cross build --release --target armv7-unknown-linux-gnueabi
+cross build --release --target arm-unknown-linux-gnueabi
